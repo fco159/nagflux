ebffe1b6ea74131fa92e482817a427eb5ce3eec6
fco159 <145263917+fco159@users.noreply.github.com>
2023-09-18 17:31:41 +0200

    Update nagiosSpoolfileWorker_test.go

--- collector/spoolfile/nagiosSpoolfileWorker_test.go
+++ collector/spoolfile/nagiosSpoolfileWorker_test.go
@@ -2,8 +2,8 @@ package spoolfile
 
 import (
 	"fmt"
-	"github.com/griesbacher/nagflux/collector"
-	"github.com/griesbacher/nagflux/helper"
+	"github.com/fco159/nagflux/collector"
+	"github.com/fco159/nagflux/helper"
 	"testing"
 )
 
