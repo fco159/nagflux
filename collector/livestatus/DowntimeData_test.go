2c76659984986b643cc2f84247f969b502cd5bff
fco159 <145263917+fco159@users.noreply.github.com>
2023-09-18 17:24:43 +0200

    Update DowntimeData_test.go

--- collector/livestatus/DowntimeData_test.go
+++ collector/livestatus/DowntimeData_test.go
@@ -2,8 +2,8 @@ package livestatus
 
 import (
 	"fmt"
-	"github.com/griesbacher/nagflux/config"
-	"github.com/griesbacher/nagflux/logging"
+	"github.com/fco159/nagflux/config"
+	"github.com/fco159/nagflux/logging"
 	"testing"
 )
 
