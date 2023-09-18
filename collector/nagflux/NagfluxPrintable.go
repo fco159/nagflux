b8ffdc8a3919487d9f6a290b8c67d51a6abe4d0e
fco159 <145263917+fco159@users.noreply.github.com>
2023-09-18 17:29:24 +0200

    Update NagfluxPrintable.go

--- collector/nagflux/NagfluxPrintable.go
+++ collector/nagflux/NagfluxPrintable.go
@@ -2,8 +2,8 @@ package nagflux
 
 import (
 	"fmt"
-	"github.com/griesbacher/nagflux/collector"
-	"github.com/griesbacher/nagflux/helper"
+	"github.com/fco159/nagflux/collector"
+	"github.com/fco159/nagflux/helper"
 )
 
 //Printable converts from nagfluxfile format to X
